c1ceb2b67014fd5bc7fd7324e3bc3303dad2107b
Anto Čabraja <anto@cabraja.(none)>
2013-03-18 14:14:33 +0100

    Probni primjer

--- proba.cu
+++ proba.cu
@@ -12,7 +12,7 @@ __global__ void VecAdd(double *A, double *B, double *C, int N)
 
 int main(int argc, char *argv[])
 {
-	int N(100);
+	int N = 100;
 	
 	size_t size = N * sizeof(double);
 
@@ -30,17 +30,17 @@ int main(int argc, char *argv[])
 	cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 	
-	 int threadsPerBlock = 20;
-   int blocksPerGrid ( (N + threadsPerBlock – 1) / threadsPerBlock );
+	int threadsPerBlock = 20;
+  int blocksPerGrid = (N + threadsPerBlock – 1) / threadsPerBlock;
    
-	 VecAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+	VecAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
 
-	 cudaFree(d_A);
-   cudaFree(d_B);
-   cudaFree(d_C);
+	cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
 
-	 free(h_A);
-	 free(h_B);
+	free(h_A);
+	free(h_B);
 
 	return 0;
 }
