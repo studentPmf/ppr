0555239043acf87156c907b28916d401abf0e268
Anto Čabraja <anto@cabraja.(none)>
2013-03-24 12:27:45 +0100

    draft

--- konjugiraniGradijenti.cu
+++ konjugiraniGradijenti.cu
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstdlib>
 #include<string>
 #include<cuda_runtime.h>
 #include<cblas.h>
@@ -16,8 +17,26 @@ void VecAdd(double* A, double* B, double* C, int N)
 }
 
 
+void procitaj(double *data, int dim, ifstream file)
+{
+	for(int i(0); i < dim; i++)
+	{
+		if(file.eof)
+		{
+			cerr<<"Greska kod citanja podataka, podatci nisu potpuni"<<endl;
+			exit ( -1 );
+		}
+		double dat;
+		file>>dat;
+		data[i] = data;
+	}
+}
+
+
 int main(int argc, char** argv)
 {
+	int dim;
+	double * A, *b, *x_0;
 	std::string datIme;
 	std::cout<<"Unesite ime tekstualne datoteke u kojoj se nalazi zadani sustav: ";
 	std::cin>>datIme;
@@ -25,7 +44,22 @@ int main(int argc, char** argv)
 	std::cout<<"Unijeli ste ime "<<datIme;
 	std::cout<<"i rezltat ce biti spremnjen u datoteku rez.txt"<<std::endl;
 
-	fstream file(datIme.c_str());
+	ifstream file( datIme.c_str() );
+	
+	if( !file.is_open() )
+	{
+		cerr<<"Greska kod otvaranja datoteke"<<endl;
+		exit( -1 );
+	}
+	
+	file>>dim;
+	*A = new int[dim*dim];
+	*b = new int[dim];
+	*x_0 = new int[dim];
+
+	procitaj(A, dim*dim, file);
+	procitaj(b, dim, file);
+	procitaj(x_0, dim, file);
 
 	return 0;
 }
