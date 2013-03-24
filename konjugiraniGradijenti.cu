680349e0c7c27dd8115232f65cab1b17c534dfc1
Anto Čabraja <anto@cabraja.(none)>
2013-03-24 12:09:29 +0100

    draft

--- konjugiraniGradijenti.cu
+++ konjugiraniGradijenti.cu
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<string>
 #include<cuda_runtime.h>
 #include<cblas.h>
+#include<fstream>
 
 __global__ 
 void VecAdd(double* A, double* B, double* C, int N)
@@ -14,5 +16,14 @@ void VecAdd(double* A, double* B, double* C, int N)
 
 int main(int argc, char** argv)
 {
+	std::string datIme;
+	std::cout<<"Unesite ime tekstualne datoteke u kojoj se nalazi zadani sustav: "
+	std::cin>>datIme;
+	std::cout<<std::endl;
+	sta::cout<<"Unijeli ste ime "<<datIme;
+	std::cout<<"i rezltat ce biti spremnjen u datoteku rez.txt"<<std::endl
+
+	fstream file(datIme.c_str());
+
 	return 0;
 }
