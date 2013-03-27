bc6a905ebff6b3a51afd8f72e96e5a1b0f7ab818
Anto Čabraja <anto@netbook.(none)>
2013-03-27 13:29:28 +0100

    draft

--- Konjugirani.c
+++ Konjugirani.c
@@ -3,14 +3,19 @@
 #include<stdlib.h>
 #include<mkl.h>
 
-int konjugiraniS(double * A, double* b, double* x_0, double* x_end, int dim)
+int konjugiraniS(double * A, double* b, double* x_0, double* x_end, int dim, int epsilon)
 {
 	int inc = 1;
 	double alpha = 1, beta = -1;
+	double tau;
 	double * d = (double*)malloc(dim*sizeof(double));
 	dgemv("No transpose",&dim,&dim,&alpha,A,&dim,x_0,&inc,&beta,b,&inc);
 	dcopy(&dim,d,&inc,b,&inc);
-	dscal(&dim,&beta,d,&inc);	
+	dscal(&dim,&beta,d,&inc);
+	while(ddot(&dim,b,&inc,b,&inc) > epsilon )
+	{
+		break;
+	}	
 	return 1;
 }
 
