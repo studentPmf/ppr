bff660b5651e1ff76b05cd05d41c5f8e058db88f
Anto Čabraja <anto@cabraja.(none)>
2013-03-28 15:18:29 +0100

    draft

--- konjugiraniGradijenti.cu
+++ konjugiraniGradijenti.cu
@@ -2,6 +2,15 @@
 #include<cstdlib>
 #include<string>
 #include<cuda_runtime.h>
-#include<cblas.h>
+#include<cublas_v2.h>
 #include "kp.h"
+using namespace std;
+
+int konjugiraniP(double* A, double* b, double* x_0, int dim, double epsilon)
+{
+	cublasHandle_h h;
+	cublasCreate(&h);
+	cublasDestroy(h);
+	return 1;
+}
 
