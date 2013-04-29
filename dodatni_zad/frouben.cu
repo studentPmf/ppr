7a79d849824e7648407342cc6adfb6c5383d098c
Anto Čabraja <cabraja.anto@gmail.com>
2013-04-29 11:57:25 +0200

    draft

--- dodatni_zad/frouben.cu
+++ dodatni_zad/frouben.cu
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+/*
+  suma elemenata po blokovima koristenjem aomic funkcije
+*/
 __global__ void funkc(int *M, int dim, unsigned int *fsum)
 {
   unsigned int rez;
@@ -26,13 +29,18 @@ __global__ void funkc(int *M, int dim, unsigned int *fsum)
    //__syncthreads();
 }
 
+/*
+   suma elemenata vektora dobivenog kao suma kvadrata elemenata blokova prethodne funkcije
+*/
 __global__ void vecAdd(unsigned int *M, int dim, int *r)
 {
   __shared__ int rez;
   rez = 0;
   __syncthreads();
+  
   int i = threadIdx.x;
   int value(0);
+  
   if(i < dim)
     value = M[i];
   
@@ -42,6 +50,8 @@ __global__ void vecAdd(unsigned int *M, int dim, int *r)
   r[0] = rez;
 }
 
+
+
 int main(int argc, char*argv[])
 {
   int N(100);
@@ -50,41 +60,48 @@ int main(int argc, char*argv[])
 
   int *M_h = (int*)malloc(size);
 
+  // geneiranje matrice
   for(int i(0); i < N*N; i++)
-    M_h[i] = 1;//i%3; // elements in the matrix is less than 3
+    M_h[i] = i%3; // elements in the matrix is less than 3
 
   int *M_d;
   cudaMalloc(&M_d, size);
   cudaMemcpy(M_d, M_h, size, cudaMemcpyHostToDevice);
 
+  // postavljanje topologije 32x32 threada u bloku i onda blokova koliko treba
   dim3 threadsPerBlock(32,32);
   dim3 blocksPerGrid((N/threadsPerBlock.x) + 1, (N/threadsPerBlock.y) + 1);
+
   int gridDimension = blocksPerGrid.x*blocksPerGrid.y;
-  cout<<blocksPerGrid.x<<","<<blocksPerGrid.y<<endl;
+
   int *result = (int*)malloc(gridDimension*sizeof(int));
   unsigned int *fsum;
   cudaMalloc(&fsum, gridDimension*sizeof(int));
-  funkc<<<blocksPerGrid, threadsPerBlock, gridDimension>>>(M_d, N, fsum);
-  cudaMemcpy(result, fsum, gridDimension*sizeof(int), cudaMemcpyDeviceToHost);
-  int *r;
-  cudaMalloc(&r, sizeof(int));
-  vecAdd<<<1, gridDimension>>>(fsum, gridDimension, r);
-  int *rezz = (int*)malloc(sizeof(int));
-  cudaMemcpy(rezz, r, sizeof(int), cudaMemcpyDeviceToHost);
 
-  cout<<endl<<"presuma = "<<rezz[0]<<endl;
-  int suma = 0;
-  for (int s(0); s < gridDimension; s++)
-  {
-    suma+=result[s];
-    cout<<"rezultat je:"<<result[s]<<endl;
-  }
+  // poziv funkcije za racunanje sume kvadrata po blokovima
+  funkc<<<blocksPerGrid, threadsPerBlock, gridDimension>>>(M_d, N, fsum);
+
+  cudaMemcpy(result, fsum, gridDimension*sizeof(int), cudaMemcpyDeviceToHost);
+  int *vectorSum;
+  cudaMalloc(&r, sizeof(int));
   
-  cout<<endl<<"Konacan rezultat je : "<<sqrt(suma)<<endl;
+  // poziv funkcije za racunanje zbroja dobivenih suma
+  vecAdd<<<1, gridDimension>>>(fsum, gridDimension, vectorSum);
+  int *sumAll = (int*)malloc(sizeof(int));
+  cudaMemcpy(sumAll, vectorSum, sizeof(int), cudaMemcpyDeviceToHost);
+
+  /*for (int s(0); s < gridDimension; s++)
+  {
+    cout<<"rezultat je:"<<result[s]<<endl;
+  }*/
+
+  cout<<endl<<"Konacan rezultat je : "<<sqrt(sumAll[0])<<endl;
 
   free(M_h);
+  free(sumAll);
   cudaFree(M_d);
   cudaFree(fsum);
+  cudaFree(r);
 
   return 0;
 }
