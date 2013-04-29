8a367c9e4534038e4dd1d7f478aeda41ba2fc906
Anto Čabraja <cabraja.anto@gmail.com>
2013-04-29 09:02:41 +0200

    dodatni

---
+++ dodatni_zad/frouben.cu
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<cuda_runtime.h>
+#include<cmath>
+
+using namespace std;
+
+__global__ void func(int *M, int dim)
+{
+  unsigned int rez;
+  __shared__ unsigned int sum;
+
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  if (i < N && j < N)
+     rez = M[N+i+j]*M[N*i+j]
+
+   __syncthreads();
+   atomicAdd(&sum, rez);
+   __syncthreads();
+}
+
+
+int main(int argc, char*argv[])
+{
+  int N(100);
+
+  size_t size = N*N*sizeof(int);
+
+  int *M_h = (int*)malloc(size);
+
+  for(int i(0); i < N*N; i++)
+    M_h[i] = i%3;
+
+  int *M_d;
+  cudaMalloc(&M_d, size);
+  cudaMemcpy(M_d, M_h, size, cudaMemcpyHostToDevice);
+
+  dim3 threadsPerBlock(16,16);
+  int blocksPerGrid = ((N / threadsPerBlock.x) + 1, (N/ threadsPerBlock.y) + 1);
+
+  funkc<<blocksPerGrid, threadsPerBlock>>>(M_d, N);
+
+  free(M_h);
+  cudaFree(M_d);
+
+  return 0;
+}
