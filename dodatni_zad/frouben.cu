13b383c433fe982eb74bc3d451c599e27aeed27f
Anto Čabraja <cabraja.anto@gmail.com>
2013-05-07 17:37:19 +0200

    none

--- dodatni_zad/frouben.cu
+++ dodatni_zad/frouben.cu
@@ -18,7 +18,7 @@ __global__ void funkc(int *M, int dim, unsigned int *fsum)
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   int j = blockIdx.y * blockDim.y + threadIdx.y;
   if (i < dim && j < dim)
-     rez = M[dim+i+j]*M[dim*i+j];
+     rez = M[dim*i+j]*M[dim*i+j];
   else
     rez = 0;
 
