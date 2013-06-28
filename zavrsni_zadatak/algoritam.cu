0b04c2ffa660b004ca68bdb25becbb46e2045fc6
Anto Čabraja <cabraja.anto@gmail.com>
2013-06-29 00:35:22 +0200

    popravak

--- zavrsni_zadatak/algoritam.cu
+++ zavrsni_zadatak/algoritam.cu
@@ -1,3 +1,7 @@
+/**
+Napisati CUDA program koji trazi maksimalni nezavisni skup vrhova
+u grafu korištenjem paralelnog algoritma koji koristi slučajne brojeve.
+*/
 #include<iostream>
 #include<stdio.h>
 #include<cstdlib>
@@ -11,15 +15,21 @@
 #include<vector>
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
+
 using namespace std;
 
 #define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
     printf("Error at %s:%d\n",__FILE__,__LINE__);\
     return EXIT_FAILURE;}} while(0)
+
 #define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
     printf("Error at %s:%d\n",__FILE__,__LINE__);\
     return EXIT_FAILURE;}} while(0)
 
+/**
+  * Host funkcija koja provjerava koliko je ostalo 
+  * neodabranih vrhova
+  */
 bool findZeros(int* polje, int n)
 {
   for(int i = 0; i < n; i++)
@@ -29,11 +39,15 @@ bool findZeros(int* polje, int n)
   return false;
 }
 
+/**
+  * Curand host funkcija. Generira pseudo-slucajne brojeve <0,1>
+  * uniformna razdioba
+  */
+
 int create_pseud_numbers(float *hostData, float *devData, int numElements)
 {
   size_t n = numElements;
   curandGenerator_t gen;
-  //float *devData;
 
   /* Create pseudo-random number generator */
   CURAND_CALL(curandCreateGenerator(&gen, 
@@ -56,28 +70,38 @@ int create_pseud_numbers(float *hostData, float *devData, int numElements)
   return EXIT_SUCCESS;
 }
 
+/**
+  * Device funkcija. Algoritam za pronalazenje maksimalnog nezavisnog skupa vrhova.
+  * Ulazni parametri : polje veza, polje pokazivaca na veze za svaki vrh po jedan pointer na polje veze,
+  *                    polje izbaceni, svaki thread zapise tko je izbacen sa -1 a ako je on trazeni postavi 1
+  */
+
 __global__ void algoritam(int* veze, int* ptr, int* izbaceni, float *devData, int* veze_size, int* ptr_size)
 {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
+  // Ako ti je index u rangu i ako nisi vec izbacen 
   if(index < *ptr_size && izbaceni[index] != -1)
   {
     int provjera = 1;
-    int start = ptr[index];
-    int end = ptr[index + 1];
+    int start = ptr[index]; //pocetak u vezama
+    int end = ptr[index + 1]; // kraj u vezama
     for(int i = start; i < end; i++)
     {
+      // Ako je netko dobio vecu tezinu i ako taj nije izbacen kao mogucnost
       if(devData[index] > devData[veze[i] - 1] && izbaceni[veze[i] - 1] != -1)
         provjera = 0;
     }
 
+    // Ako je prosao provjeru
     if(provjera)
     {
-       izbaceni[index] = 1;
+       izbaceni[index] = 1; // postavi da je index dobar
        for(int i = start; i< end; i++)
-         izbaceni[veze[i] - 1 ] = -1;
+         izbaceni[veze[i] - 1 ] = -1; // sve susjede izbaci kao mogucnost
     }
   }
 }
+
 
 int main(int argc, const char* argv[])
 {
@@ -88,15 +112,12 @@ int main(int argc, const char* argv[])
     return EXIT_FAILURE;
   }
 
-  /* Citanje iz datoteke
-     Sve se sprema u vektor
-     Ime datoteke se cita s kom. linije
-  */
-  int numElements;
-  vector<int> indElements; // vektor veza za sve vrhove, format v1v20v1v3v40...
-  vector<int> ptrVector;
+  int numElements; // broj vrhova
+  vector<int> indElements; // vektor susjedstva ( veze )
+  vector<int> ptrVector; // pointeri vrhova u vektoru susjedstva
   ifstream myFile (argv[1]);
   
+  // Ako je file dobar prepisi ga u vektore
   if(myFile.is_open())
   {
     int cnt = 0;
@@ -122,6 +143,9 @@ int main(int argc, const char* argv[])
     cerr<<"Pogresno ime datoteke"<<endl;
     return EXIT_FAILURE;
   }
+
+  /* Provjera da li je sve procitano korektno*/
+  /*
   for(int i(0); i < ptrVector.size(); i++)
     cout<<ptrVector[i];
   cout<<endl;
@@ -129,31 +153,39 @@ int main(int argc, const char* argv[])
   for(int i(0); i < indElements.size(); i++)
     cout<<indElements[i];
   cout<<endl;
-  //********************************************//
+  */
   
-  int* HindElements = &indElements[0]; // vektor elemenata
-  int* HptrVector = &ptrVector[0];     // vektor pointera na pocetak za svaki vrh
+  /* Priprema za device*/
+  /*****************************************************************/
+
+  int* HindElements = &indElements[0]; // iz vektora u polje
+  int* HptrVector = &ptrVector[0];     // iz vektora u polje
   int Hizbaceni[numElements];
+  // Inicijalno sve na 0 jer su svi vrhovi raspolozivi za koristenje
   for(int i(0); i < numElements; i++)
     Hizbaceni[i] = 0;
 
-  float * hostData, *devData;
-  int Hveze_size = indElements.size(), Hptr_size = ptrVector.size();
-  int *Dveze_size, *Dptr_size;
-    /* Allocate n floats on host */
+  float * hostData, *devData; // polja za pseudo-slucajne brojeve
+  
+  // alokacija za generator pseudo brojeva
   hostData = (float *)calloc(numElements, sizeof(float));
-    /* Allocate n floats on device */
   CUDA_CALL(cudaMalloc((void **)&devData, numElements*sizeof(float)));
   
   create_pseud_numbers(hostData, devData, numElements);
   
-  /* Show result */
+  /* Prikaz rezultata */
+  /*
   for( int i = 0; i < numElements; i++) {
     printf("%1.4f ", hostData[i]);
   }
   cout<<endl;
-  
-  int *DindElements,*DptrVector, *Dizbaceni;
+  */
+  // Alokacija memorija za glavni program (algoritam)
+  int Hveze_size = indElements.size(), Hptr_size = ptrVector.size(); // pomocne varijable  
+  int *Dveze_size, *Dptr_size;
+  int *DindElements, *DptrVector, *Dizbaceni;
+  int izbaceni[numElements];
+
   CUDA_CALL(cudaMalloc((void **)&DindElements, indElements.size()*sizeof(int)));
   CUDA_CALL(cudaMalloc((void **)&DptrVector, ptrVector.size()*sizeof(int)));
   CUDA_CALL(cudaMalloc((void **)&Dizbaceni, numElements*sizeof(int)));
@@ -171,18 +203,22 @@ int main(int argc, const char* argv[])
         cudaMemcpyHostToDevice));
   CUDA_CALL(cudaMemcpy(Dptr_size, &Hptr_size, sizeof(int),
         cudaMemcpyHostToDevice));
-  
-  int izbaceni[numElements];
-
+ 
+  // Algoritam
   do{
-    algoritam<<<1,numElements>>>(DindElements, DptrVector, Dizbaceni, devData, Dveze_size, Dptr_size);
-  CUDA_CALL(cudaMemcpy(izbaceni, Dizbaceni, numElements * sizeof(int),
-        cudaMemcpyDeviceToHost));
+    
+    algoritam<<<numElements/32 + 1,32>>>(DindElements, DptrVector, Dizbaceni, devData, Dveze_size, Dptr_size);
+    CUDA_CALL(cudaMemcpy(izbaceni, Dizbaceni, numElements * sizeof(int),
+              cudaMemcpyDeviceToHost));
+
   }while(findZeros(izbaceni, numElements));
+
+  // ispisi matrice odabranih i izbacenih vrhova 1 -> odabrani, -1 -> izbaceni
   for( int k = 0; k < numElements; k++)
     cout<<izbaceni[k];
   cout<<endl;
 
+  // Oslobadanje memorije na hostu i divace-u 
   free(hostData);
   CUDA_CALL(cudaFree(devData));
   CUDA_CALL(cudaFree(DindElements));
@@ -190,4 +226,6 @@ int main(int argc, const char* argv[])
   CUDA_CALL(cudaFree(Dizbaceni));
   CUDA_CALL(cudaFree(Dveze_size));
   CUDA_CALL(cudaFree(Dptr_size));
+
+  return 0;
 }
