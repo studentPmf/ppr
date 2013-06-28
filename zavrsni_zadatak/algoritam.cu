88ce96764f9f9562f4bf8aae8ada56d070117c49
Anto Čabraja <cabraja.anto@gmail.com>
2013-06-29 01:22:13 +0200

    popravak

--- zavrsni_zadatak/algoritam.cu
+++ zavrsni_zadatak/algoritam.cu
@@ -208,7 +208,7 @@ int main(int argc, const char* argv[])
   // Algoritam
   do{
     
-    algoritam<<<numElements/32,32>>>(DindElements, DptrVector, Dizbaceni, devData, Dveze_size, Dptr_size);
+    algoritam<<<numElements/32 + 1,32>>>(DindElements, DptrVector, Dizbaceni, devData, Dveze_size, Dptr_size);
     CUDA_CALL(cudaMemcpy(izbaceni, Dizbaceni, numElements * sizeof(int), cudaMemcpyDeviceToHost));
 
   }while(findZeros(izbaceni, numElements));
