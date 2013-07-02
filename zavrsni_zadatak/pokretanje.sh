f6fc88617d066b461af5eae21ebd634c9cf2b7e4
Anto Čabraja <cabraja.anto@gmail.com>
2013-07-02 08:51:40 +0200

    priprema testiranja

--- zavrsni_zadatak/pokretanje.sh
+++ zavrsni_zadatak/pokretanje.sh
@@ -3,10 +3,10 @@ echo kompajliranje ...
 nvcc -arch=sm_20 algoritam.cu pseudo_generator/mttest.cpp pseudo_generator/mtrand/mtrand.cpp -lcurand
 echo kompajliranje zavrseno
 echo Pokretanje na grafovima
-./a.out graf1.txt
-./a.out graf2.txt
-./a.out graf3.txt
-./a.out graf4.txt
-./a.out graf5.txt
-./a.out graf5.txt
-./a.out graf7.txt
+./a.out testni_primjeri/graf1.txt
+./a.out testni_primjeri/graf2.txt
+./a.out testni_primjeri/graf3.txt
+./a.out testni_primjeri/graf4.txt
+./a.out testni_primjeri/graf5.txt
+./a.out testni_primjeri/graf5.txt
+./a.out testni_primjeri/graf7.txt
